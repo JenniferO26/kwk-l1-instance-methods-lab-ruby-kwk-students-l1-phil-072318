0e4815b060499a2ad5e7fb8666d1a87cd7c85ebc
Jennifer Okwara <jokwara03@gmail.com>
2018-07-26 20:32:21 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,23 @@
 # person.rb
+class Dog 
+   def initialize(sit, bark)
+    @talk = talk
+     @sit = sit
+   end
+  def return_bark
+    @talk
+    puts "Woof!"
+   end
+ def return_sit
+   @walk
+   puts "The person is walking."
+ end
+ end
+  
+  george = Dog.new("george")
+  
+puts george.return_sit
+  
+  
+ 
+ 
